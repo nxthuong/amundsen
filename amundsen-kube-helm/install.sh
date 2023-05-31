462a093e2114847a77b3ce1943f4bcb52705fe1e
Thuong.NguyenXuan <thuong.nguyenxuan@MB031.local>
2023-05-31 15:05:39 +0700

    Update helm chart

---
+++ amundsen-kube-helm/install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+set -e
+
+# GCP - Container Registry setup
+gcloud auth login
+gcloud config set project baemin-vietnam
+gcloud auth configure-docker gcr.io
+
+# Build Docker images
+docker build -f Dockerfile.frontend.public -t gcr.io/baemin-vietnam/amundsen-frontend:dev .
+docker push gcr.io/baemin-vietnam/amundsen-frontend:dev
+
+docker build -f Dockerfile.metadata.public -t gcr.io/baemin-vietnam/amundsen-metadata:dev .
+docker push gcr.io/baemin-vietnam/amundsen-metadata:dev
+
+docker build -f Dockerfile.search.public -t gcr.io/baemin-vietnam/amundsen-search:dev .
+docker push gcr.io/baemin-vietnam/amundsen-search:dev
+
+# GCP - K8s cluster
+gcloud container clusters get-credentials data-service-nat --zone asia-southeast1-a --project baemin-vietnam
+kubectl create namespace amundsen
+
+# Helm chart
+cd ./amundsen-kube-helm
+helm repo add elasticsearch https://helm.elastic.co
+helm dependency build ./templates/helm/
+helm install my-amundsen ./templates/helm/ --namespace amundsen
